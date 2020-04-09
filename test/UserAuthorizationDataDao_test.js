87159d2558fbd52c651f91edf5c74aa4415fd2b3
Eric Oseid <ericoseid@pop-os.localdomain>
2020-04-08 22:07:55 -0700

    Some more unit tests

--- test/UserAuthorizationDataDao_test.js
+++ test/UserAuthorizationDataDao_test.js
@@ -1,10 +1,14 @@
 const assert = require('assert');
 const sinon = require('sinon');
-
 const mysql = require('mysql');
-const {retrieveAuthorizationData} =
+const {storeAuthorizationData,
+       retrieveAuthorizationData,
+       updateAuthorizationToken} =
   require('../persistence/UserAuthorizationDataDao.js');
 
+const USER = 'eric';
+const ACCESS_TOKEN = 'accessToken';
+const REFRESH_TOKEN = 'refreshToken';
 const dbConnectionInfo = {
   host : 'localhost',
   user : 'ericoseid',
@@ -31,8 +35,46 @@ afterEach(() => {
   mysql.createConnection.restore();
 });
 
+describe('storeAuthorizationData', () => {
+  const query = `INSERT INTO userAuthorizationInfo
+                (user, authorizationToken, refreshToken) VALUES
+                (\'${USER}\', \'${ACCESS_TOKEN}\', \'${REFRESH_TOKEN}\');`
+
+  describe('when the query fails', () => {
+    it('passes an error to the callback', (done) => {
+      createConnection.withArgs(dbConnectionInfo).returns(connection);
+      connection.query
+                .withArgs(query, sinon.match.func)
+                .yields('error', undefined, undefined);
+
+      storeAuthorizationData(USER, ACCESS_TOKEN, REFRESH_TOKEN,
+                             (error) => {
+        assert.ok(error);
+
+        done();
+      });
+    });
+  })
+
+  describe('when the query succeeds', () => {
+    it('passes a list containing the result to the callback', (done) => {
+      createConnection.withArgs(dbConnectionInfo).returns(connection);
+      connection.query
+                .withArgs(query, sinon.match.func)
+                .yields(undefined, ['row'], undefined);
+
+      storeAuthorizationData(USER, ACCESS_TOKEN, REFRESH_TOKEN,
+                             (error) => {
+        assert.ok(!error);
+
+        done();
+      });
+    });
+  });
+});
+
 describe('retrieveAuthorizationData', () => {
-  const query = "SELECT * FROM userAuthorizationInfo WHERE user=\'eric\';"
+  const query = `SELECT * FROM userAuthorizationInfo WHERE user=\'${USER}\';`
 
   describe('when the query fails', () => {
     it('passes an error to the callback', (done) => {
@@ -51,15 +93,51 @@ describe('retrieveAuthorizationData', () => {
   });
 
   describe('when the query succeeds', () => {
-    it('passes a list containing the resule to the callback', (done) => {
+    it('passes a list containing the result to the callback', (done) => {
       createConnection.withArgs(dbConnectionInfo).returns(connection);
       connection.query
                 .withArgs(query, sinon.match.func)
                 .yields(undefined, ['row'], undefined);
 
-      retrieveAuthorizationData('eric', (error, row) => {
+      retrieveAuthorizationData(USER, (error, row) => {
         assert.ok(!error);
         assert.strictEqual('row', row[0]);
+
+        done();
+      });
+    });
+  });
+});
+
+describe('updateAuthorizationToken', () => {
+  const query = `UPDATE userAuthorizationInfo SET
+                authorizationToken='${ACCESS_TOKEN}'
+                WHERE user='${USER}';`;
+
+  describe('when the query fails', () => {
+    it('passes an error to the callback', (done) => {
+      createConnection.withArgs(dbConnectionInfo).returns(connection);
+      connection.query
+                .withArgs(query, sinon.match.func)
+                .yields('error', undefined, undefined);
+
+      updateAuthorizationToken(USER, ACCESS_TOKEN, (error) => {
+        assert.ok(error);
+
+        done();
+      });
+    });
+  })
+
+  describe('when the query succeeds', () => {
+    it('passes a list containing the result to the callback', (done) => {
+      createConnection.withArgs(dbConnectionInfo).returns(connection);
+      connection.query
+                .withArgs(query, sinon.match.func)
+                .yields(undefined, ['row'], undefined);
+
+      updateAuthorizationToken(USER, ACCESS_TOKEN, (error) => {
+        assert.ok(!error);
 
         done();
       });
